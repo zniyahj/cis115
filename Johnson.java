d97e88e36088e6cae12cb46e0a2bc275805ef353
zniyahj <johnsonz1512@student.faytechcc.edu>
2025-02-18 11:57:34 -0500

    Add files via upload

---
+++ Johnson.java
@@ -0,0 +1,11 @@
+// import the java utility Scanner to get input from the keyboard
+import java.util.Scanner;
+public class Johnson 
+{
+
+    public static void main(String[] args) 
+    {
+        // declare Scanner variable to get input from the keyboard
+       Scanner keyboard = new Scanner(System.in);
+    }
+}
