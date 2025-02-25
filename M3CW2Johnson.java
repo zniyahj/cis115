11fcfde057ae9df55c98440878199ba8a85d7817
zniyahj <johnsonz1512@student.faytechcc.edu>
2025-02-25 11:01:15 -0500

    Add files via upload

---
+++ M3CW2Johnson.java
@@ -0,0 +1,10 @@
+public class M3CW2Johnson 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.print("Programming ");
+        System.out.print("is ");
+        System.out.print("fun.");
+    }
+}
